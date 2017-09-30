5e5f0d222e66b635132332b9444d2c2531941208
proutyio <kyle@prouty.io>
2017-09-30 16:58:31 -0700

    small refactor, cleaned up console output, console output now shows which consumer is consuming and what producer is producing

--- Concurrency_1/Producer_Consumer.c
+++ Concurrency_1/Producer_Consumer.c
@@ -29,32 +29,30 @@ struct Buffer {
 struct Buffer buffer[FULL];
 
 void* producer(void *x);
-void* consumer();
+void* consumer(void *x);
 int random_int();
 void debug_print(int x); //REMOVE ME
 
 
 int main() 
 {
-	int current_producer[THREADS];
-
+	int p[THREADS];
+	int c[THREADS];
 	for(int i=0; i<THREADS; i++) {
-		current_producer[i] = i;
-		pthread_create(&p_id[i], NULL, producer, &current_producer[i]);
-		pthread_create(&c_id[i], NULL, consumer, NULL);
+		p[i] = i;
+		c[i] = i;
+		pthread_create(&p_id[i], NULL, producer, &p[i]);
+		pthread_create(&c_id[i], NULL, consumer, &c[i]);
 	}
-
 	for(int i=0; i<THREADS; i++)
 		pthread_join(c_id[i], NULL);
-
 	return 0;
 }
 
 
 void* producer(void *x)
 {	
-	int current_producer = *((int *) x);
-	
+	int producer = *((int *) x);
 	while(1) {
 		if(pthread_mutex_trylock(&mutex) == 0) {
 			sleep(random_int(3,7));
@@ -66,7 +64,7 @@ void* producer(void *x)
 			buffer[items].item = random_int(0,10);
 			buffer[items].sleep = random_int(2,9);
 			
-			debug_print(current_producer); //REMOVE ME
+			debug_print(producer); //REMOVE ME
 			
 			items++;
 			pthread_mutex_unlock(&mutex);
@@ -76,8 +74,9 @@ void* producer(void *x)
 }
 
 
-void* consumer()
+void* consumer(void *x)
 {
+	int consumer = *((int *) x);
 	while(1) {
 		if(pthread_mutex_trylock(&mutex) == 0) {
 			while(items == EMPTY) {
@@ -85,7 +84,7 @@ void* consumer()
 				pthread_mutex_unlock(&mutex);
 			} 
 			sleep(buffer[items-1].sleep); 
-			printf("%s%d\n","Consuming: ",buffer[items-1].item);
+			printf("%s%i%s\t%d\n","(",consumer,")Consumed:",buffer[items-1].item);
 			items--;
 			pthread_cond_broadcast(&p_cond);
 			pthread_mutex_unlock(&mutex);
@@ -104,6 +103,6 @@ int random_int(int min, int max)
 
 void debug_print(int x) //REMOVE ME
 {
-	printf("buffer: %d\n", items);
-	printf("%s%i%s\t%d\t%d\n","Producer ",x,":",buffer[items].item, buffer[items].sleep); 
+	printf("buffer:\t%d\n", items);
+	printf("%s%i%s\t%d\t%d\n","(",x,")Produced:",buffer[items].item, buffer[items].sleep); 
 }
