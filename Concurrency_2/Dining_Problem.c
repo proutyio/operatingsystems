4d2134c2338f3a01d69a3ad3e722dea2e320e077
proutyio <kyle@prouty.io>
2017-10-23 01:09:02 -0700

    initial setup. basic c file setup. makefile created.

---
+++ Concurrency_2/Dining_Problem.c
@@ -0,0 +1,17 @@
+/*
+*	Kyle Prouty
+*	Nathaniel Whitlock
+*
+*	Operating Systems 2 - Fall 2017/2018
+*	Concurrency Problem 2
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() 
+{
+	return 0;
+}
