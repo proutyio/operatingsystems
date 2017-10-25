97fceb8afef8d72f857ca72ba74e4fa87a14f39f
proutyio <kyle@prouty.io>
2017-10-24 22:19:26 -0700

    deleted a file that didnt belong anymore, more code setup. pesudo code written and using as a base for code.

--- Concurrency_2/Dining_Philosophers.c
+++ Concurrency_2/Dining_Philosophers.c
@@ -4,87 +4,46 @@
 *
 *	Operating Systems 2 - Fall 2017/2018
 *	Concurrency Problem 2
-*	-- Extra Credit --	
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 #include <unistd.h>
-#include <cpuid.h>
-#include "mt19937ar.c"
 
-#define THREADS 10
-#define FULL 32
-#define EMPTY 0 
+
 /*
  * Philosophers:
  * - Immanuel_Kant
  * - Friedrich_Nietzsche
  * - Rene_Descartes
  * - Thomas_Aquinas 
+ * - John_Locke
  */
 
 
-pthread_t p_id[THREADS];
-pthread_t c_id[THREADS];
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t p_cond = PTHREAD_COND_INITIALIZER;
-pthread_cond_t c_cond = PTHREAD_COND_INITIALIZER;
-int items = 0;
-int check = 0;
+pthread_t id[5];
+pthread_mutex_t fork_1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t fork_2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t fork_3 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t fork_4 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t fork_5 = PTHREAD_MUTEX_INITIALIZER;
 
-struct Buffer {
-	int item;
-	int sleep;
-}; 
-struct Buffer buffer[FULL];
 
-void* producer(void *x);
-void* consumer(void *x);
-void check_rdrand();
-int random_int();
-int rdrand();
-void display();
+
+void* philosopher(void *x);
+
+void rand_wait(int min,int max);
+
 
 
 int main() 
 {
-	check_rdrand();
-	display();
-	int p[THREADS];
-	int c[THREADS];
-	for(int i=0; i<THREADS; i++) {
-		p[i] = i;
-		c[i] = i;
-		pthread_create(&p_id[i], NULL, producer, &p[i]);
-		pthread_create(&c_id[i], NULL, consumer, &c[i]);
-	}
-	for(int i=0; i<THREADS; i++)
-		pthread_join(c_id[i], NULL);
+	
 	return 0;
 }
 
-void* producer(void *x)
-{	
-	int producer = *((int *) x);
-	while(1) {
-		if(pthread_mutex_trylock(&mutex) == 0) {
-			sleep(random_int(3,7));
-			while(items == FULL) {
-				pthread_cond_wait(&p_cond, &mutex);
-				pthread_mutex_unlock(&mutex);
-			}
-			buffer[items].item = random_int(0,10);
-			buffer[items].sleep = random_int(2,9);
-			printf("%s%i\n%s%i%s\t%d\t%d\n","buffer:\t",items,
-				"(",producer,")Produced:",buffer[items].item, buffer[items].sleep); 
-			items++;
-			pthread_mutex_unlock(&mutex);
-			pthread_cond_broadcast(&c_cond);
-		}
-	}
-}
+
 
 void* philosopher(void *x)
 {
@@ -100,58 +59,4 @@ void* philosopher(void *x)
 	
 }
 
-void* consumer(void *x)
-{
-	int consumer = *((int *) x);
-	while(1) {
-		if(pthread_mutex_trylock(&mutex) == 0) {
-			while(items == EMPTY) {
-				pthread_cond_wait(&c_cond, &mutex);
-				pthread_mutex_unlock(&mutex);
-			} 
-			sleep(buffer[items-1].sleep); 
-			printf("%s%i%s\t%d\n","(",consumer,")Consumed:",buffer[items-1].item);
-			items--;
-			pthread_cond_broadcast(&p_cond);
-			pthread_mutex_unlock(&mutex);
-		}
-	}
-}
 
-void check_rdrand()
-{
-	unsigned int a,b,c,d, reg = (1 << 30);
-	__cpuid(1, a, b, c, d);
-	if( ((c & reg) == reg) == 1)
-		check = 1;
-	else 
-		check = 0;
-}
-
-int random_int(int min, int max)
-{
-	int x = 0;
-	if(check == 1) 
-		x = abs(rdrand() % ((max+1) - min)) + min;
-	else 
-		x = (genrand_int32() % ((max-3) + min)) + min;
-	return x;
-}
-
-int rdrand()
-{
-	unsigned int x;
-	__asm__("rdrand  %[x]": [x] "=r" (x) :: "cc");
-	return x;	
-}
-
-void display()
-{	
-	char* s;
-	if(check == 1)
-		s = "YES";
-	else
-		s = "NO";
-	printf("%s\n%s%i\n%s%i\n%s%s\n%s\n","-----","Producers: ",THREADS,
-		"Consumers: ",THREADS,"RDRand supported?: ",s,"-----");
-}
