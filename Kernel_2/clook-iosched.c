a0d6c4d97c15c423ac590fa5de689407cdccd003
natewhit44 <natewhit44@gmail.com>
2017-10-29 10:47:42 -0700

    Organized insert of new requests based on disk sectors

--- Kernel_2/clook-iosched.c
+++ Kernel_2/clook-iosched.c
@@ -26,13 +26,25 @@ static void clook_merged_requests(struct request_queue *q, struct request *rq,
   list_del_init(&next->queuelist);
 }
 
+/*
+ * Added print statement to monitor request dispatch
+ */
 static int clook_dispatch(struct request_queue *q, int force)
 {
   struct clook_data *cd = q->elevator->elevator_data;
+  char *op;
 
   if (!list_empty(&cd->queue)) {
     struct request *rq;
     rq = list_entry(nd->queue.next, struct request, queuelist);
+    
+    if (rq_data_dir(rq))
+      op = "Write";
+    else
+      op = "Read";
+
+    printk("\n%s%s %llu\n", "[CLOOK] dsp ", op, (unsigned long long)blk_rq_pos(rq));
+
     list_del_init(&rq->queuelist);
     elv_dispatch_sort(q, rq);
     return 1;
@@ -40,9 +52,60 @@ static int clook_dispatch(struct request_queue *q, int force)
   return 0;
 }
 
+/*
+ * Following changes were made:
+ * - Added print statement to monitor request addition
+ * - Controlled insert to order requests in reguards to disk sector  
+ */
 static void clook_add_request(struct request_queue *q, struct request *rq)
 {
   struct clook_data *cd = q->elevator->elevator_data;
+  struct list_head *pos = NULL;
+  struct request *next_req, *cur_req, *prev_req;
+
+  sector_t cur_sector, rq_sector, next_sector;
+  char *op;
+
+  /*
+   * Check for read or write operation
+   */
+  if (rq_data_dir(rq))
+    op = "Write";
+  else
+    op = "Read";
+
+  /*
+   * Print status message
+   */
+  printk("\n%s%s%s%llu\n", "[CLOOK] add ", op, " ", (unsigned long long)blk_rq_pos(rq));
+  list_for_each(pos, &nd->queue){
+    next_req = list_entry(pos->next, struct request, queuelist);
+    prev_req = list_entry(pos->prev, struct request, queuelist);
+    cur_req = list_entry(pos, struct request, queuelist);
+
+    next_sector = blk_rq_pos(next_req);
+    rq_sector = blk_rq_pos(rq);
+    cur_sector = blk_rq_pos(cur_req);
+
+    printk("\n%s\n%llu%s%llu%s%s\n", "cur_sector > rq_sector", (unsigned long long)cur_sector,
+	   "  >  ", (unsigned long long)rq_sector, " = ", ((cur_sector > rq_sector) ? "TRUE" : "FALSE"));
+
+    /*
+     * Check to see if the current sector in the queue is larger than the request
+     * sector, if this is true break and insert on the tail side. This keeps the largest
+     * request sector before the head position so that the head can proceed through the
+     * circular doubly linked list to smaller sectors once the large have been processed,
+     * thus created a circular motion
+     */
+    if(cur_sector > rq_sector){
+      printk("\n%s\n%s%llu\n%s%llu\n%s%llu\n", "In for loop (BREAK):", "Next sector: ",
+	     (unsigned long long)blk_rq_pos(next_req),
+	     "Current sector: ", (unsigned long long)blk_rq_pos(cur_req),
+	     "Prev sector: ", (unsigned long long)blk_rq_pos(prev_req));
+
+      break;
+    }
+  }
 
   list_add_tail(&rq->queuelist, &cd->queue);
 }
@@ -67,6 +130,9 @@ clook_latter_request(struct request_queue *q, struct request *rq)
   return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
+/*
+ * Added print statement to mark scheduler initialization
+ */
 static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
 {
   struct clook_data *cd;
